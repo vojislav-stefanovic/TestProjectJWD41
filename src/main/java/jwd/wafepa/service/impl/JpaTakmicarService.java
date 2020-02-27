229f630c1c6130dcb4e2b0275ef96891ad32f7a6
vojas <emojvojo@gmail.com>
2020-02-27 22:07:40 +0100

    update2

--- src/main/java/jwd/wafepa/service/impl/JpaTakmicarService.java
+++ src/main/java/jwd/wafepa/service/impl/JpaTakmicarService.java
@@ -14,40 +14,42 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class JpaTakmicarService implements TakmicarService {
 
-	private @NonNull TakmicarRepository takmicarRepository;
+    private @NonNull TakmicarRepository takmicarRepository;
 
-	@Override
-	public Takmicar findOne(Long id) {
-		return takmicarRepository.findOne(id);
-	}
+    @Override
+    public Takmicar findOne(Long id) {
+        return takmicarRepository.findOne(id);
+    }
 
-	@Override
-	public Takmicar save(Takmicar t) {
-		return takmicarRepository.save(t);
-	}
+    @Override
+    public Takmicar save(Takmicar t) {
+        return takmicarRepository.save(t);
+    }
 
-	@Override
-	public Takmicar delete(Long id) {
-		Takmicar t = takmicarRepository.findOne(id);
-		takmicarRepository.delete(id);
-		return t;
-	}
+    @Override
+    public Takmicar delete(Long id) {
+        Takmicar t = takmicarRepository.findOne(id);
+        takmicarRepository.delete(id);
+        return t;
 
-	@Override
-	public Page<Takmicar> findAll(int page) {
-		return takmicarRepository.findAll(new PageRequest(page, 6));
-	}
 
-	@Override
-	public Page<Takmicar> pretraga(String imeIprezime, String drzava, Integer minGodiste, Integer maxGodiste,
-			int page) {
-		if (imeIprezime != null) {
-			imeIprezime = "%" + imeIprezime + "%";
-		}
-		if (drzava != null) {
-			drzava = "%" + drzava + "%";
-		}
-		return takmicarRepository.pretraga(imeIprezime, drzava, minGodiste, maxGodiste, new PageRequest(page, 6));
-	}
+    }
+
+    @Override
+    public Page<Takmicar> findAll(int page) {
+        return takmicarRepository.findAll(new PageRequest(page, 6));
+    }
+
+    @Override
+    public Page<Takmicar> pretraga(String imeIprezime, String drzava, Integer minGodiste, Integer maxGodiste,
+                                   int page) {
+        if (imeIprezime != null) {
+            imeIprezime = "%" + imeIprezime + "%";
+        }
+        if (drzava != null) {
+            drzava = "%" + drzava + "%";
+        }
+        return takmicarRepository.pretraga(imeIprezime, drzava, minGodiste, maxGodiste, new PageRequest(page, 6));
+    }
 
 }
